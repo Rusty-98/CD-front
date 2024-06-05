7c92873d3d5da1b1bc3488947e655508903960a0
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-06-05 23:12:26 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
 export const initSocket = async () => {
-    return io("https://cd-back.onrender.com/");
-    // return io("https://cd-back-one.vercel.app/");
+    return io("https://cd-back-one.vercel.app/",{
+        withCredentials: true,
+    }
+        );
+    // return io("https://cd-back.onrender.com/");
 }
