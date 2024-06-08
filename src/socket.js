a7e24bb878ad5ba5eb68e24ff946f90655d7585d
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-06-08 12:48:51 +0530

    Refactor socket.js to use the production server URL

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
 export const initSocket = async () => {
-    return io("https://cd-back-one.vercel.app/",{
-        transports: ['websocket', 'polling']
-    });
-    // return io("https://cd-back.onrender.com/");
+    // return io("https://cd-back-one.vercel.app/",{
+    //     transports: ['websocket', 'polling']
+    // });
+    return io("https://cd-back.onrender.com/");
 }
